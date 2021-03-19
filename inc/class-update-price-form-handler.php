6ab0da6c66436f24b5eea8797f4e687353e3d07f
Bart Jaskulski <bjaskulski@protonmail.com>
2021-03-19 11:18:32 +0100

    Translate string to pl

--- inc/class-update-price-form-handler.php
+++ inc/class-update-price-form-handler.php
@@ -52,7 +52,7 @@ class Update_Price_Form_Handler implements Component_Interface {
 	 */
 	public function action_update_cart_button_after_form() {
 		?>
-		<button class='is-disabled has-background' id="update_cart"><?php esc_html_e( 'Update cart', 'asysdent-theme' ); ?></button>
+		<button class='is-disabled has-background' id="update_cart"><?php esc_html_e( 'Zaktualizuj cenę', 'asysdent-theme' ); ?></button>
 		<?php
 	}
 
